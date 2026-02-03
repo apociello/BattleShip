f46ce0a4cb2b57cf915a113c4d4b917d214568b0
pociello <asierpociello@gmail.com>
2026-02-03 08:20:26 +0100

    feat: handle incorrect individual ship placement

--- src/board.js
+++ src/board.js
@@ -4,6 +4,16 @@ class Board {
   }
 
   placeShip(ship, coordinate, axis) {
+    if (
+      coordinate.x < 0 ||
+      coordinate.y < 0 ||
+      coordinate.x > 9 ||
+      coordinate.y > 9
+    )
+      return;
+    if (axis === "x" && coordinate.y + (ship.size - 1) > 9) return;
+    if (axis === "y" && coordinate.x - (ship.size - 1) < 0) return;
+
     if (axis === "x") {
       for (let i = 0; i < ship.size; i++) {
         this.board[coordinate.x][coordinate.y + i] = "O";
